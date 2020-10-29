46dbe94f9250cef104aa41e74ec1bfe0e33836da
EadkennyChan <jgw@JGWdeMac-mini.local>
2020-10-29 17:58:11 +0800

    修改默认404提示

--- server/src/config/middleware.js
+++ server/src/config/middleware.js
@@ -21,7 +21,10 @@ module.exports = [
     handle: 'trace',
     enable: !think.isCli,
     options: {
-      debug: isDev
+      debug: isDev,
+      templates: {
+        404: path.join(think.ROOT_PATH, 'view/error_404.html')
+      }
     }
   },
   {
--- server/src/controller/base.js
+++ server/src/controller/base.js
@@ -2,4 +2,7 @@ module.exports = class extends think.Controller {
   __before() {
 
   }
+  __call() {
+    return this.fail(404, "该接口不存在");
+  }
 };
